650a8a2ac846d09fafd5f4b65d186d6b479e0c46
Andrew Renninger <awrenninger@crimson.ua.edu>
2023-04-21 10:43:06 -0500

    prolog

---
+++ a8/functions.pl
@@ -0,0 +1,60 @@
+list(Op, Ref, List) :-
+    ( Op = ('==') -> findall([X,Y,Z], (pay(X,Y, Z), Z =:= Ref), List);
+      Op = ('!=') -> findall([X,Y,Z], (pay(X,Y, Z), Z =\= Ref), List);
+      Op = ('>') -> findall([X,Y,Z], (pay(X,Y, Z), Z > Ref), List);
+      Op = ('>=') -> findall([X,Y,Z], (pay(X,Y, Z), Z >= Ref), List);
+      Op = ('<') -> findall([X,Y,Z], (pay(X,Y, Z), Z < Ref), List);
+      Op = ('<=') -> findall([X,Y,Z], (pay(X,Y, Z), Z =< Ref), List)
+    ).
+
+count(Op, Ref, Count) :-
+    ( Op = ('==') -> findall(Z, (pay(_, _, Z), Z =:= Ref), List);
+      Op = ('!=') -> findall(Z, (pay(_, _, Z), Z =\= Ref), List);
+      Op = ('>') -> findall(Z, (pay(_, _, Z), Z > Ref), List);
+      Op = ('>=') -> findall(Z, (pay(_, _, Z), Z >= Ref), List);
+      Op = ('<') -> findall(Z, (pay(_, _, Z), Z < Ref), List);
+      Op = ('<=') -> findall(Z, (pay(_, _, Z), Z =< Ref), List)
+    ),
+    length(List, Count).
+
+min(Op, Ref, Min) :-
+    ( Op = ('==') -> findall(Z, (pay(_, _, Z), Z =:= Ref), List);
+      Op = ('!=') -> findall(Z, (pay(_, _, Z), Z =\= Ref), List);
+      Op = ('>') -> findall(Z, (pay(_, _, Z), Z > Ref), List);
+      Op = ('>=') -> findall(Z, (pay(_, _, Z), Z >= Ref), List);
+      Op = ('<') -> findall(Z, (pay(_, _, Z), Z < Ref), List);
+      Op = ('<=') -> findall(Z, (pay(_, _, Z), Z =< Ref), List)
+    ),
+    min_list(List, Min).
+
+max(Op, Ref, Max) :-
+    ( Op = ('==') -> findall(Z, (pay(_, _, Z), Z =:= Ref), List);
+      Op = ('!=') -> findall(Z, (pay(_, _, Z), Z =\= Ref), List);
+      Op = ('>') -> findall(Z, (pay(_, _, Z), Z > Ref), List);
+      Op = ('>=') -> findall(Z, (pay(_, _, Z), Z >= Ref), List);
+      Op = ('<') -> findall(Z, (pay(_, _, Z), Z < Ref), List);
+      Op = ('<=') -> findall(Z, (pay(_, _, Z), Z =< Ref), List)
+    ),
+    max_list(List, Max).
+
+total(Op, Ref, Total) :-
+    ( Op = ('==') -> findall(Z, (pay(_, _, Z), Z =:= Ref), List);
+      Op = ('!=') -> findall(Z, (pay(_, _, Z), Z =\= Ref), List);
+      Op = ('>') -> findall(Z, (pay(_, _, Z), Z > Ref), List);
+      Op = ('>=') -> findall(Z, (pay(_, _, Z), Z >= Ref), List);
+      Op = ('<') -> findall(Z, (pay(_, _, Z), Z < Ref), List);
+      Op = ('<=') -> findall(Z, (pay(_, _, Z), Z =< Ref), List)
+    ),
+    sum_list(List, Total).
+
+avg(Op, Ref, Avg) :-
+    ( Op = ('==') -> findall(Z, (pay(_, _, Z), Z =:= Ref), List);
+      Op = ('!=') -> findall(Z, (pay(_, _, Z), Z =\= Ref), List);
+      Op = ('>') -> findall(Z, (pay(_, _, Z), Z > Ref), List);
+      Op = ('>=') -> findall(Z, (pay(_, _, Z), Z >= Ref), List);
+      Op = ('<') -> findall(Z, (pay(_, _, Z), Z < Ref), List);
+      Op = ('<=') -> findall(Z, (pay(_, _, Z), Z =< Ref), List)
+    ),
+    sum_list(List, Total),
+    length(List, Len),
+    Avg is Total / Len.
---
+++ a8/rules.pl
@@ -0,0 +1,11 @@
+pay(First, Last, Pay) :- salaried(First, Last, Salary),
+    Pay is Salary.
+
+pay(First, Last, Pay) :- hourly(First, Last, Hours, Rate),
+    (Hours =< 40 -> Pay is Hours * Rate ;
+    Hours =< 50 -> Pay is 40 * Rate + (Hours - 40) * Rate * 1.5;
+    Pay is 40 * Rate + 10 * Rate * 1.5 + (Hours - 50) * Rate * 2).
+
+pay(First, Last, Pay) :- commission(First, Last, MinSalary, Sales, Rate),
+    Commission is Sales * Rate,
+    (Commission < MinSalary -> Pay is MinSalary; Pay is Commission).
